cb5b2fc35872800f2cd22684d734760399ea3fd1
Igor Martayan <igor@martayan.org>
2023-06-13 11:47:47 +0200

    Update main

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,32 @@ mod kmer;
 mod lyndon;
 mod reads;
 mod utils;
-use kmer::Kmer;
-use lyndon::Lyndon;
+// use kmer::Kmer;
+// use lyndon::Lyndon;
 use utils::*;
 
-pub const K: usize = 5;
+pub const K: usize = 11;
 pub type T = u32;
 
 fn main() {
-    // all_lmers().iter().for_each(|&lmer| print_kmer_nucs(lmer));
+    // all_lmers().iter().for_each(|&lmer| print_kmer_bin(lmer));
+    all_lmers()
+        .iter()
+        .for_each(|&lmer| println!("{:022b}", lmer.to_int()));
+
     // let kmers = random_kmers(10);
     // kmers.iter().for_each(|&kmer| print_kmer_nucs(kmer));
     // println!();
     // kmers
     //     .iter()
     //     .for_each(|&kmer| print_kmer_nucs(kmer.necklace()));
-    let seq = b"GCTCACGC";
-    Kmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_nucs(kmer));
-    println!();
-    Kmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_nucs(kmer.necklace()));
-    println!();
-    Kmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_bin(kmer));
-    println!();
-    Kmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_bin(kmer.necklace()));
+
+    // let seq = b"GCTCACGC";
+    // Kmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_nucs(kmer));
+    // println!();
+    // Kmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_nucs(kmer.necklace()));
+    // println!();
+    // Kmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_bin(kmer));
+    // println!();
+    // Kmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_bin(kmer.necklace()));
 }
