3c68aa9b57b518c32024b5025b33102729bcb8f5
Igor Martayan <igor@martayan.org>
2023-07-10 12:00:56 +0200

    Standalone function for computing necklace

--- src/lyndon.rs
+++ src/lyndon.rs
@@ -1,16 +1,45 @@
 use crate::kmer::{Base, Kmer};
 use core::cmp::min;
 
+#[inline]
+pub fn bin_rot_right<const N: usize, T: Base>(x: T) -> T {
+    ((x & T::one()) << (N - 1)) | (x >> 1)
+}
+
+pub fn necklace<const N: usize, T: Base>(x: T) -> T {
+    let mut res = x;
+    let mut rot = x;
+    for _ in 1..N {
+        rot = bin_rot_right::<N, T>(rot);
+        res = min(res, rot);
+    }
+    res
+}
+
+pub fn necklace_index<const N: usize, T: Base>(x: T) -> (T, usize) {
+    let mut res = x;
+    let mut rot = x;
+    let mut idx = 0;
+    for i in 1..N {
+        rot = bin_rot_right::<N, T>(rot);
+        if rot < res {
+            res = rot;
+            idx = i;
+        }
+    }
+    (res, idx)
+}
+
 pub trait Lyndon<const K: usize, T: Base>: Kmer<K, T> {
     #[inline]
-    fn rot_right(x: T) -> T {
+    fn bin_rot_right(x: T) -> T {
         ((x & T::one()) << (2 * K - 2)) | (x >> 1)
     }
     fn lmer(self) -> T {
         let mut res = self.canonical().to_int() >> 1;
         let mut rot = res;
         for _ in 1..(2 * K - 1) {
-            rot = Self::rot_right(rot);
+            rot = Self::bin_rot_right(rot);
             res = min(res, rot);
         }
         res
@@ -20,7 +49,7 @@ pub trait Lyndon<const K: usize, T: Base>: Kmer<K, T> {
         let mut idx = 0;
         let mut rot = res;
         for i in 1..(2 * K - 1) {
-            rot = Self::rot_right(rot);
+            rot = Self::bin_rot_right(rot);
             if rot < res {
                 res = rot;
                 idx = i;
