4d7040e77fefbc5212e78d5c42d27aafc49abf00
Igor Martayan <igor@martayan.org>
2023-07-19 17:10:59 +0200

    Move main to sample

--- examples/sample.rs
+++ examples/sample.rs
@@ -1,33 +1,24 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-mod kmer;
-mod lyndon;
-mod rank;
-mod reads;
-mod utils;
-
-use kmer::{Kmer, RawKmer};
-use lyndon::Lyndon;
-use rank::Ranker;
-use utils::*;
+use lmer::kmer::{Kmer, RawKmer};
+use lmer::lyndon::Lyndon;
+use lmer::rank::Ranker;
+use lmer::utils::*;
+use std::fs::File;
+use std::io::Write;
 
 // const K: usize = 15;
 // type T = u32;
 // type L = u32;
 // const B: usize = 5;
-// type RB = roaring::RoaringBitmap;
 
 const K: usize = 19;
 type T = u64;
 type L = u32;
 const B: usize = 6;
-type RB = roaring::RoaringTreemap;
 
 // const K: usize = 31;
 // type T = u64;
 // type L = u64;
 // const B: usize = 6;
-// type RB = roaring::RoaringTreemap;
 
 // const K: usize = 35;
 // type T = u128;
@@ -39,21 +30,10 @@ const N: usize = 2 * K - 1;
 type TSet = std::collections::BTreeSet<T>;
 type LSet = std::collections::BTreeSet<L>;
 
-use std::fs::File;
-use std::io::Write;
-
-use sucds::mii_sequences::EliasFanoBuilder;
-use sucds::Serializable;
-
 fn main() {
     let mut set_kmers_cp = TSet::new();
     let mut set_lmers_idx = TSet::new();
     let mut set_ranks = LSet::new();
-
-    // let mut rb_kmers = RB::new();
-    // let mut rb_kmers_cp = RB::new();
-    // let mut rb_lmers = RB::new();
-    // let mut rb_lmers_idx = RB::new();
 
     let kmers = random_kmers::<K, T, KT>(1_000_000);
     let ranker = Ranker::<N, T>::new();
@@ -66,11 +46,6 @@ fn main() {
         set_kmers_cp.insert(kmer_cp);
         set_lmers_idx.insert(rank_idx);
         set_ranks.insert(rank as L);
-
-        // rb_kmers.insert(kmer.to_int());
-        // rb_kmers_cp.insert(kmer_cp);
-        // rb_lmers.insert(rank);
-        // rb_lmers_idx.insert(rank_idx);
     }
 
     let mut out_kmers = File::create(format!("sorted_kmers_{}.txt", K)).unwrap();
@@ -87,9 +62,4 @@ fn main() {
     set_ranks
         .iter()
         .for_each(|x| writeln!(out_ranks, "{}", x).unwrap());
-
-    // println!("Kmer bitmap: {} bytes", rb_kmers.serialized_size());
-    // println!("Kmer cp bitmap: {} bytes", rb_kmers_cp.serialized_size());
-    // println!("Lmer bitmap: {} bytes", rb_lmers.serialized_size());
-    // println!("Lmer idx bitmap: {} bytes", rb_lmers_idx.serialized_size());
 }
