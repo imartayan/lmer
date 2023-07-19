c2a32568292b408d7da95f2eafc2f66822b836b1
Igor Martayan <igor@martayan.org>
2023-07-19 12:16:05 +0200

    Update main

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,32 @@ use lyndon::Lyndon;
 use rank::Ranker;
 use utils::*;
 
-pub type T = u32;
-const K: usize = 15;
-const B: usize = 5;
-type RB = roaring::RoaringBitmap;
+// const K: usize = 15;
+// pub type T = u32;
+// pub type L = u32;
+// const B: usize = 5;
+// type RB = roaring::RoaringBitmap;
 
-// pub type T = u64;
+const K: usize = 19;
+pub type T = u64;
+pub type L = u32;
+const B: usize = 6;
+type RB = roaring::RoaringTreemap;
+
 // const K: usize = 31;
+// pub type T = u64;
+// pub type L = u64;
 // const B: usize = 6;
 // type RB = roaring::RoaringTreemap;
 
+// const K: usize = 35;
+// pub type T = u128;
+// pub type L = u64;
+// const B: usize = 7;
+
 const N: usize = 2 * K - 1;
 type TSet = std::collections::BTreeSet<T>;
+type LSet = std::collections::BTreeSet<L>;
 
 use std::fs::File;
 use std::io::Write;
@@ -33,6 +47,7 @@ use sucds::Serializable;
 fn main() {
     let mut set_kmers_cp = TSet::new();
     let mut set_lmers_idx = TSet::new();
+    let mut set_ranks = LSet::new();
 
     // let mut rb_kmers = RB::new();
     // let mut rb_kmers_cp = RB::new();
@@ -49,6 +64,7 @@ fn main() {
 
         set_kmers_cp.insert(kmer_cp);
         set_lmers_idx.insert(rank_idx);
+        set_ranks.insert(rank as L);
 
         // rb_kmers.insert(kmer.to_int());
         // rb_kmers_cp.insert(kmer_cp);
@@ -56,43 +72,23 @@ fn main() {
         // rb_lmers_idx.insert(rank_idx);
     }
 
-    // let mut out_kmers = File::create("sorted_kmers.txt").unwrap();
-    // set_kmers_cp
-    //     .iter()
-    //     .for_each(|x| writeln!(out_kmers, "{}", x).unwrap());
+    let mut out_kmers = File::create(format!("sorted_kmers_{}.txt", K)).unwrap();
+    set_kmers_cp
+        .iter()
+        .for_each(|x| writeln!(out_kmers, "{}", x).unwrap());
 
-    // let mut out_lmers = File::create("sorted_lmers.txt").unwrap();
-    // set_lmers_idx
-    //     .iter()
-    //     .for_each(|x| writeln!(out_lmers, "{}", x).unwrap());
+    let mut out_lmers = File::create(format!("sorted_lmers_{}.txt", K)).unwrap();
+    set_lmers_idx
+        .iter()
+        .for_each(|x| writeln!(out_lmers, "{}", x).unwrap());
 
-    // set_lmers_idx.iter().for_each(|x| println!("{}", x))
+    let mut out_ranks = File::create(format!("sorted_ranks_{}.txt", K)).unwrap();
+    set_ranks
+        .iter()
+        .for_each(|x| writeln!(out_ranks, "{}", x).unwrap());
 
     // println!("Kmer bitmap: {} bytes", rb_kmers.serialized_size());
     // println!("Kmer cp bitmap: {} bytes", rb_kmers_cp.serialized_size());
     // println!("Lmer bitmap: {} bytes", rb_lmers.serialized_size());
     // println!("Lmer idx bitmap: {} bytes", rb_lmers_idx.serialized_size());
-
-    // let mut efb = EliasFanoBuilder::new(1 << (N + 1), set_kmers_cp.len())
-    //     .expect("Failed to create Elias-Fano Builder");
-    // efb.extend(set_kmers_cp.iter().map(|&x| x as usize))
-    //     .unwrap();
-    // let ef = efb.build();
-    // println!("Kmer cp EF: {} bytes", ef.size_in_bytes());
-
-    // let mut efb = EliasFanoBuilder::new(1 << (N + 1), set_lmers_idx.len())
-    //     .expect("Failed to create Elias-Fano Builder");
-    // efb.extend(set_lmers_idx.iter().map(|&x| x as usize))
-    //     .unwrap();
-    // let ef = efb.build();
-    // println!("Lmer idx EF: {} bytes", ef.size_in_bytes());
-
-    // let seq = b"GCTCACGC";
-    // RawKmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_nucs(kmer));
-    // println!();
-    // RawKmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_nucs(kmer.necklace()));
-    // println!();
-    // RawKmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_bin(kmer));
-    // println!();
-    // RawKmer::<K, T>::iter_from_nucs(seq.iter()).for_each(|kmer| print_kmer_bin(kmer.necklace()));
 }
