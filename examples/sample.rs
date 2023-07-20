07af5205965fa97a3a00bf7a3ea9979a88907006
Igor Martayan <igor@martayan.org>
2023-07-20 11:47:01 +0200

    Use constants for sampling

--- examples/sample.rs
+++ examples/sample.rs
@@ -1,3 +1,4 @@
+use lmer::constants::{LmerT, CANON_BITS, K, ROT_BITS, T};
 use lmer::kmer::{Kmer, RawKmer};
 use lmer::lyndon::Lyndon;
 use lmer::rank::Ranker;
@@ -5,30 +6,9 @@ use lmer::utils::*;
 use std::fs::File;
 use std::io::Write;
 
-// const K: usize = 15;
-// type T = u32;
-// type L = u32;
-// const B: usize = 5;
-
-const K: usize = 19;
-type T = u64;
-type L = u32;
-const B: usize = 6;
-
-// const K: usize = 31;
-// type T = u64;
-// type L = u64;
-// const B: usize = 6;
-
-// const K: usize = 35;
-// type T = u128;
-// type L = u64;
-// const B: usize = 7;
-
 type KT = RawKmer<K, T>;
-const N: usize = 2 * K - 1;
 type TSet = std::collections::BTreeSet<T>;
-type LSet = std::collections::BTreeSet<L>;
+type LSet = std::collections::BTreeSet<LmerT>;
 
 fn main() {
     let mut set_kmers_cp = TSet::new();
@@ -36,16 +16,16 @@ fn main() {
     let mut set_ranks = LSet::new();
 
     let kmers = random_kmers::<K, T, KT>(1_000_000);
-    let ranker = Ranker::<N, T>::new();
+    let ranker = Ranker::<CANON_BITS, T>::new();
     for kmer in kmers {
         let kmer_cp = kmer.canonical().to_int() >> 1;
         let (lmer, idx) = kmer.lmer_index();
         let rank = ranker.rank(lmer);
-        let rank_idx = (rank << B) | idx as T;
+        let rank_idx = (rank << ROT_BITS) | idx as T;
 
         set_kmers_cp.insert(kmer_cp);
         set_lmers_idx.insert(rank_idx);
-        set_ranks.insert(rank as L);
+        set_ranks.insert(rank as LmerT);
     }
 
     let mut out_kmers = File::create(format!("sorted_kmers_{}.txt", K)).unwrap();
