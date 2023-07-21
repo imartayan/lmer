905849b387b239b665f08a2c12ffbde51b4812d3
Igor Martayan <igor@martayan.org>
2023-07-21 16:01:40 +0200

    Factorize necklace functions

--- examples/graph.rs
+++ examples/graph.rs
@@ -19,22 +19,22 @@ fn main() {
     let mut set_necks = HashSet::new();
     for _ in 0..1000 {
         kmer = ((kmer << 1) | rng.gen_range(0..2)) & MASK;
-        set_necks.insert(necklace::<N, _>(kmer));
+        set_necks.insert(necklace::<K, _>(kmer));
     }
     println!("{} necklaces", set_necks.len());
 
-    let mut dot = File::create("sample.dot").unwrap();
+    let mut dot = File::create("graph_necklaces_bin.dot").unwrap();
     writeln!(dot, "digraph {{ node[shape=point]").unwrap();
 
     for &neck in set_necks.iter() {
         let mut rot = neck;
         for _ in 0..N {
             let s = (rot >> 1) | ((1 - (rot & 1)) << (N - 1));
-            let ns = necklace::<N, _>(s);
+            let ns = necklace::<K, _>(s);
             if set_necks.contains(&ns) {
                 writeln!(dot, "{:b} -> {:b}", neck, ns).unwrap();
             }
-            rot = bin_rot_right::<N, _>(rot);
+            rot = bin_rot_right::<K, _>(rot);
         }
     }
 
--- src/lyndon.rs
+++ src/lyndon.rs
@@ -2,26 +2,26 @@ use crate::kmer::{Base, Kmer};
 use core::cmp::min;
 
 #[inline]
-pub fn bin_rot_right<const N: usize, T: Base>(x: T) -> T {
-    ((x & T::one()) << (N - 1)) | (x >> 1)
+pub fn bin_rot_right<const K: usize, T: Base>(x: T) -> T {
+    ((x & T::one()) << (2 * K - 2)) | (x >> 1)
 }
 
-pub fn necklace<const N: usize, T: Base>(x: T) -> T {
+pub fn necklace<const K: usize, T: Base>(x: T) -> T {
     let mut res = x;
     let mut rot = x;
-    for _ in 1..N {
-        rot = bin_rot_right::<N, T>(rot);
+    for _ in 1..(2 * K - 1) {
+        rot = bin_rot_right::<K, T>(rot);
         res = min(res, rot);
     }
     res
 }
 
-pub fn necklace_index<const N: usize, T: Base>(x: T) -> (T, usize) {
+pub fn necklace_index<const K: usize, T: Base>(x: T) -> (T, usize) {
     let mut res = x;
     let mut rot = x;
     let mut idx = 0;
-    for i in 1..N {
-        rot = bin_rot_right::<N, T>(rot);
+    for i in 1..(2 * K - 1) {
+        rot = bin_rot_right::<K, T>(rot);
         if rot < res {
             res = rot;
             idx = i;
@@ -31,31 +31,11 @@ pub fn necklace_index<const N: usize, T: Base>(x: T) -> (T, usize) {
 }
 
 pub trait Lyndon<const K: usize, T: Base>: Kmer<K, T> {
-    #[inline]
-    fn bin_rot_right(x: T) -> T {
-        ((x & T::one()) << (2 * K - 2)) | (x >> 1)
-    }
     fn lmer(self) -> T {
-        let mut res = self.canonical().to_int() >> 1;
-        let mut rot = res;
-        for _ in 1..(2 * K - 1) {
-            rot = Self::bin_rot_right(rot);
-            res = min(res, rot);
-        }
-        res
+        necklace::<K, T>(self.canonical().to_int() >> 1)
     }
     fn lmer_index(self) -> (T, usize) {
-        let mut res = self.canonical().to_int() >> 1;
-        let mut idx = 0;
-        let mut rot = res;
-        for i in 1..(2 * K - 1) {
-            rot = Self::bin_rot_right(rot);
-            if rot < res {
-                res = rot;
-                idx = i;
-            }
-        }
-        (res, idx)
+        necklace_index::<K, T>(self.canonical().to_int() >> 1)
     }
 }
 
