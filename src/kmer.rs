28e760770a78f3b958188a4bba1cd045f1baaf0c
Igor Martayan <igor@martayan.org>
2023-11-24 17:13:56 +0100

    Improve Kmer trait

--- src/kmer.rs
+++ src/kmer.rs
@@ -1,4 +1,5 @@
 use core::fmt::{Binary, Display};
+use core::hash::Hash;
 use core::iter::FilterMap;
 use num_traits::int::PrimInt;
 
@@ -13,7 +14,7 @@ pub trait RevComp {
     fn rev_comp(self) -> Self;
 }
 
-pub trait Kmer<const K: usize, T: Base>: Sized + Copy + RevComp {
+pub trait Kmer<const K: usize, T: Base>: Sized + Copy + RevComp + Ord + Hash {
     const MASK: T;
     fn from_int(s: T) -> Self;
     fn to_int(self) -> T;
@@ -30,8 +31,16 @@ pub trait Kmer<const K: usize, T: Base>: Sized + Copy + RevComp {
         Self::from_int(((self.to_int() << 2) | base) & Self::MASK)
     }
     #[inline]
+    fn prepend(self, base: T) -> Self {
+        Self::from_int((self.to_int() >> 2) | (base << (2 * (K - 1))))
+    }
+    #[inline]
     fn successors(self) -> [Self; 4] {
         T::bases().map(|base| self.append(base))
+    }
+    #[inline]
+    fn predecessors(self) -> [Self; 4] {
+        T::bases().map(|base| self.prepend(base))
     }
     #[inline]
     fn is_canonical(self) -> bool {
@@ -45,20 +54,30 @@ pub trait Kmer<const K: usize, T: Base>: Sized + Copy + RevComp {
             self.rev_comp()
         }
     }
-    fn from_nucs(nucs: &[u8]) -> Self {
-        nucs.iter()
-            .filter_map(T::from_nuc)
-            .take(K)
-            .fold(Self::new(), |s, base| s.extend(base))
+    #[inline]
+    fn from_bases_iter<I: Iterator<Item = T>>(bases: I) -> Self {
+        bases.take(K).fold(Self::new(), |s, base| s.extend(base))
     }
-    fn to_nucs(self) -> [u8; K] {
-        let mut res = [0u8; K];
+    #[inline]
+    fn from_bases(bases: &[T]) -> Self {
+        Self::from_bases_iter(bases.iter().copied())
+    }
+    fn to_bases(self) -> [T; K] {
+        let mut res = [T::zero(); K];
         let mut s = self.to_int();
         for i in 0..K {
-            res[K - 1 - i] = (s & T::BASE_MASK).to_nuc();
+            res[K - 1 - i] = s & T::BASE_MASK;
             s = s >> 2;
         }
         res
+    }
+    #[inline]
+    fn from_nucs(nucs: &[u8]) -> Self {
+        Self::from_bases_iter(nucs.iter().filter_map(T::from_nuc))
+    }
+    #[inline]
+    fn to_nucs(self) -> [u8; K] {
+        self.to_bases().map(|base| base.to_nuc())
     }
     fn iter_from_bases<I: Iterator<Item = T>>(bases: I) -> KmerIterator<K, T, Self, I> {
         KmerIterator {
@@ -67,10 +86,12 @@ pub trait Kmer<const K: usize, T: Base>: Sized + Copy + RevComp {
             init: false,
         }
     }
+    #[inline]
+    #[allow(clippy::type_complexity)]
     fn iter_from_nucs<'a, I: Iterator<Item = &'a u8>>(
         nucs: I,
     ) -> KmerIterator<K, T, Self, FilterMap<I, fn(&u8) -> Option<T>>> {
-        Self::iter_from_bases(nucs.filter_map(<T>::from_nuc))
+        Self::iter_from_bases(nucs.filter_map(T::from_nuc))
     }
 }
 
@@ -123,9 +144,7 @@ macro_rules! impl_t {
         #[inline]
         fn to_nuc(self) -> u8 {
             const BASE_LOOKUP: [u8; 4] = [b'A', b'C', b'T', b'G'];
-            if self >= 4 {
-                panic!("Invalid base")
-            }
+            debug_assert!(self < 4, "Invalid base");
             BASE_LOOKUP[self as usize]
         }
         #[inline(always)]
